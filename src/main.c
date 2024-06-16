088c8b87e6962fb2c7f76a7771fa64424235eb4e
LucianRadul <radlucradulescu@gmail.com>
2024-06-16 15:41:17 +0200

    add tokenizer

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 
 void print_prompt1()
 {
-    fprintf(stderr, "! ");
+    fprintf(stderr, "frozesh> ");
 }
 
 void print_prompt2(void)
@@ -64,14 +64,51 @@ char * read_command(void)
 
         if(chunk[chunk_length - 1] == '\n')
         {
-            // If users pressed ENTER then return command
-            return command;
+            if(chunk[chunk_length - 2] != '\\') 
+            {
+                // If user pressed ENTER and the command 
+                // is not multi-line with BACKSLASH
+                // then return the command
+                return command;
+            }
+            
+            // If the command is multi-line with BACKSLASH
+            // then print the second prompt
+            print_prompt2();
         }
 
         command_length += chunk_length;
     }
 
     return command;
+}
+
+char ** tokenizer(char * cmd)
+{
+    char ** tokens;
+    char * token;
+    const char * delimiters;
+    unsigned int count;
+
+    tokens = malloc(sizeof(cmd));
+    delimiters = " \n\\";
+    count = 0;
+
+    token = strtok(cmd, delimiters);
+
+    if(token == NULL)
+    {
+        fprintf(stderr, "Error: failed to tokenize, command is empty %s\n", strerror(errno));
+    }
+
+    do
+    {
+        tokens[count] = token;
+        token = strtok(NULL, delimiters);
+        count += 1;
+    } while (token != NULL);
+
+    return tokens;
 }
 
 int main(int argc, char **argv)
@@ -84,7 +121,7 @@ int main(int argc, char **argv)
         cmd = read_command();
 
         // If there is nothing in the command line then exit.
-        if (!cmd) 
+        if (! cmd) 
         {
             exit(EXIT_SUCCESS);
         }
@@ -103,8 +140,19 @@ int main(int argc, char **argv)
             break;
         }
 
-        printf("%s", cmd); // Placeholder. Print back the introduced command.
-        free(cmd);
+        char ** tokens;
+        tokens = tokenizer(cmd);
+
+        for (int i = 0; i < 2; i ++)
+        {
+            char * token = tokens[i];
+            printf("%s\n", token);
+        }
+        
+        // Placeholder. Print back the introduced command.
+        // printf("%s", cmd);
+
+        free(cmd); // Command was used, hence free
     }
 
     exit(EXIT_SUCCESS);
